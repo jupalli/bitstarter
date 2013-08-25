162749f972c7a4e8ea1a283d0b77d5df2e0704ae
Ubuntu <ubuntu@ip-172-31-43-73.us-west-2.compute.internal>
2013-08-25 22:34:12 +0000

    Hello World2

--- web.js
+++ web.js
@@ -2,21 +2,9 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var buffer;// = new Buffer("Hello World", "utf-8");
-var fs = require('fs');
-
-fs.readFile('index.html', function (err, data) {
-response.send("Reading the file");
-/*if (err) console.log("Error reading the file");
-else 
-buffer = new Buffer(data, "utf-8");*/
-} 
-);
-
-/*app.get('/', function(request, response) {
-//  response.send('Hello World2!');
-  //response.send(buffer.toString());
-});*/
+app.get('/', function(request, response) {
+  response.send('Hello World2!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
