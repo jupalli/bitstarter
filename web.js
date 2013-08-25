c0992d55a84bac1ffa7d2ec4f65d179ad29d786b
Ubuntu <ubuntu@ip-172-31-43-73.us-west-2.compute.internal>
2013-08-25 22:06:09 +0000

    Changed web.js to read from index.html

--- web.js
+++ web.js
@@ -2,8 +2,17 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buffer;// = new Buffer("Hello World", "utf-8");
+
+fs.readFile('index.html', function (err, data) {
+if (err) console.log("Error reading the file");
+else 
+buffer = new Buffer(data, "utf-8");} 
+);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
