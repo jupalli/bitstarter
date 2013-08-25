5391dfc7262537b77047a5bc84e7e22011ab5f16
Ubuntu <ubuntu@ip-172-31-43-73.us-west-2.compute.internal>
2013-08-25 22:53:57 +0000

    Reading indeex.html

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var buf;// = new Buffer("Hello World", "utf-8");
+var buffer;// = new Buffer("Hello World", "utf-8");
 var fs = require('fs');
 
 fs.readFile('index.html', function (err, data) {
@@ -13,13 +13,13 @@ console.log("Error reading the file");
 }
 else
 {
-buf = new Buffer(data, "utf-8");
+buffer = new Buffer(data, "utf-8");
 } 
 });
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
-  response.send(buf.toString());
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
