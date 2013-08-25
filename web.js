9312bde2cbb62cdb486bd2d49d72c1735e0a9b8e
Ubuntu <ubuntu@ip-172-31-43-73.us-west-2.compute.internal>
2013-08-25 22:36:08 +0000

    Buffer created

--- web.js
+++ web.js
@@ -6,6 +6,9 @@ app.get('/', function(request, response) {
   response.send('Hello World2!');
 });
 
+var fs = require('fs');
+var buf;
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
