0d6ebc5064623b601ad04619f021e29b54288052
Ubuntu <ubuntu@ip-172-31-43-73.us-west-2.compute.internal>
2013-08-25 22:42:05 +0000

    Reading indeex.html

--- web.js
+++ web.js
@@ -15,6 +15,11 @@ fs.readFile('index.html', function (err, data) {
   buf = new Buffer(data);
 });
 
+app.get('/', function(request, response) {
+  response.send('Hello World2!');
+});
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
